a066057e70690b9ab28e4b530e7bfaa39ca8edcc
Ayberk Demirkol <ayberk.demirkol@hotmail.com>
2020-10-20 19:46:58 +0300

    Initial Commit

---
+++ TodoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by Ayberk Demirkol on 20/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
